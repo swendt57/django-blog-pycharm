6efd9b304d9e690c1701b911bea1bf0659306796
swendt57 <Lthr4ever>
2020-02-01 09:11:09 -0800

    finished adding templates

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from .views import get_posts, post_detail, create_or_edit_post
 
 urlpatterns = [
     url(r'^$', get_posts, name='get_posts'),
-    url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'), # reg expression for an ID
+    url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'),  # reg expression for an ID
     url(r'^new/$', create_or_edit_post, name='new_post'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_post, name='edit_post')
 ]
